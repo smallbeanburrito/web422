865c6f168af3aa45bc0fb6d0d444e7e5b73dc462
Keith <kcao13@myseneca.ca>
2023-06-02 18:47:17 -0400

    update declaration

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB422 – Assignment 2
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Keith Cao Student ID: 1443332211 Date: June 2, 2023 
+*  Render Link: https://assignment2-dr3j.onrender.com
+*
+********************************************************************************/ 
+
 var page = 1;
 var perPage = 10;
 
